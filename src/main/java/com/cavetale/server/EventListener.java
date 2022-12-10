02e42763cb3ebf73b772cef156736e2a97547a48
StarTux <ths.loy@googlemail.com>
2022-12-10 12:17:49 +0100

    Print player list in player list header

--- src/main/java/com/cavetale/server/EventListener.java
+++ src/main/java/com/cavetale/server/EventListener.java
@@ -1,5 +1,6 @@
 package com.cavetale.server;
 
+import com.cavetale.core.command.RemotePlayer;
 import com.cavetale.core.connect.Connect;
 import com.cavetale.core.event.hud.PlayerHudEvent;
 import com.cavetale.core.event.hud.PlayerHudPriority;
@@ -17,20 +18,25 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
+import static com.cavetale.core.font.Unicode.subscript;
 import static com.cavetale.core.font.Unicode.tiny;
 import static net.kyori.adventure.text.Component.empty;
 import static net.kyori.adventure.text.Component.join;
+import static net.kyori.adventure.text.Component.space;
 import static net.kyori.adventure.text.Component.text;
-import static net.kyori.adventure.text.JoinConfiguration.noSeparators;
+import static net.kyori.adventure.text.Component.textOfChildren;
+import static net.kyori.adventure.text.JoinConfiguration.separator;
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 @RequiredArgsConstructor
 public final class EventListener implements Listener {
     private final ServerPlugin plugin;
     private final List<Component> sidebarLines = new ArrayList<>();
+    private final List<Component> whoLines = new ArrayList<>();
 
     public void enable() {
         Bukkit.getPluginManager().registerEvents(this, plugin);
+        Bukkit.getScheduler().runTaskTimer(plugin, this::makeWhoLines, 0L, 20L);
     }
 
     /**
@@ -81,15 +87,41 @@ public final class EventListener implements Listener {
         }
     }
 
+    private void makeWhoLines() {
+        whoLines.clear();
+        for (ServerSlot slot : plugin.serverMap.values()) {
+            slot.onlinePlayers.clear();
+        }
+        for (RemotePlayer remote : Connect.get().getRemotePlayers()) {
+            ServerSlot slot = plugin.serverMap.get(remote.getOriginServerName());
+            if (slot == null) continue;
+            slot.onlinePlayers.add(remote);
+        }
+        for (ServerSlot slot : plugin.getServerList()) {
+            List<RemotePlayer> playerList = slot.onlinePlayers;
+            if (playerList.size() == 0) continue;
+            Collections.sort(playerList, (a, b) -> String.CASE_INSENSITIVE_ORDER.compare(a.getName(), b.getName()));
+            Component nameComponent = textOfChildren(text(subscript(slot.flatDisplayName.toLowerCase() + "(" + playerList.size() + ")"), GRAY));
+            List<Component> playerNames = new ArrayList<>();
+            for (RemotePlayer online : playerList) {
+                playerNames.add(text(online.getName()));
+            }
+            whoLines.add(textOfChildren(nameComponent, space(), join(separator(space()), playerNames)));
+        }
+    }
+
     @EventHandler
     private void onPlayerHud(PlayerHudEvent event) {
         if (!sidebarLines.isEmpty()) {
             event.sidebar(PlayerHudPriority.INFO, sidebarLines);
         }
-        event.header(PlayerHudPriority.HIGHEST,
-                     List.of(DefaultFont.CAVETALE.forPlayer(event.getPlayer()),
-                             join(noSeparators(), text(tiny("server "), GRAY), (plugin.serverSlot != null ? plugin.serverSlot.displayName : empty())),
-                             join(noSeparators(), text(tiny("players "), GRAY), text(Connect.get().getOnlinePlayerCount(), WHITE))));
+        final Player player = event.getPlayer();
+        List<Component> header = new ArrayList<>();
+        header.add(DefaultFont.CAVETALE.forPlayer(player));
+        header.add(textOfChildren(text(tiny("server "), GRAY), (plugin.serverSlot != null ? plugin.serverSlot.displayName : empty())));
+        header.add(textOfChildren(text(tiny("players "), GRAY), text(Connect.get().getOnlinePlayerCount(), WHITE)));
+        header.addAll(whoLines);
+        event.header(PlayerHudPriority.HIGHEST, header);
     }
 
     protected void updateSidebarLines() {
